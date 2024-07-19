ba71257f3718eea7546400d22292e0179ddd520a
lubiano83 <lubiano83@gmail.com>
2024-07-19 16:27:56 -0400

    paginado de productos listo

--- src/router/app.cart.routes.js
+++ src/router/app.cart.routes.js
@@ -7,10 +7,7 @@ const CART = new CartManager();
 ROUTER.get("/", async (req, res) => {
     try {
         const allCarts = await CART.getCarts();
-        return res.status(200).render("carts", {
-            title: "Carts",
-            carts: allCarts,
-        });
+        res.status(200).render("carts", { title: "Carts", carts: allCarts });
     } catch (error) {
         res.status(500).send(error.message);
         res.status(500).json({ status: false, message: "Hubo un error en el servidor" });
