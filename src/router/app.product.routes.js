be11a94c1d184abdbab8c3be3396ec5bf4f55050
lubiano83 <lubiano83@gmail.com>
2024-07-23 12:22:24 -0400

    boton agregar producto al primer carrito del array carritos listo

--- src/router/app.product.routes.js
+++ src/router/app.product.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import ProductManager from "../controllers/ProductManager.js";
+import CartManager from "../controllers/CartManager.js";
 
 const ROUTER = Router();
 const PRODUCT = new ProductManager();
+const CART = new CartManager();
 
 ROUTER.get("/", async (req, res) => {
     try {
@@ -22,6 +24,38 @@ ROUTER.get("/:id", async (req, res) => {
             return res.status(404).send("<h1>Producto no encontrado</h1>");
         }
         return res.status(200).render("productDetail", { title: "Product Detail", product: product });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("<h1>Hubo un error en el servidor</h1>");
+    }
+});
+
+ROUTER.post("/:id/add-to-cart", async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const carts = await CART.getCarts();
+
+        if (carts.length === 0) {
+            await CART.addCart();
+            const carts = await CART.getCarts();
+            const firstCartId = carts[0]._id;
+            await CART.addProductToCart(firstCartId, productId);
+            return res.redirect(`/carts/${firstCartId}`);
+        }
+
+        const firstCartId = carts[0]._id;
+        console.log(firstCartId);
+        const result = await CART.addProductToCart(firstCartId, productId);
+
+        if (result === "Producto no encontrado") {
+            return res.status(404).json({ status: false, message: result });
+        }
+
+        if (result === "Carrito no encontrado") {
+            return res.status(404).json({ status: false, message: result });
+        }
+
+        return res.redirect(`/carts/${firstCartId}`);
     } catch (error) {
         console.log(error.message);
         res.status(500).send("<h1>Hubo un error en el servidor</h1>");
