018d62891e1ed1585fe637503fb2ae1d4cd293a6
lubiano83 <lubiano83@Mac-mini-de-Jose.local>
2024-10-12 18:16:47 -0300

    ajustes varios

--- src/public/index.js
+++ src/public/index.js
@@ -28,7 +28,9 @@ SOCKET.on("products", (products) => {
                     <td class="code update" id="${product._id}">${product.code}</td>
                     <td class="category update" id="${product._id}">${product.category}</td>
                     <td class="title update" id="${product._id}">${product.title}</td>
-                    <td class="thumbnail update" id="${product._id}"><a href="${product.thumbnail[0]}" target="_blank">${product.thumbnail[0]}</a></td>
+                    <td class="thumbnail update" id="${product._id}"><a href="${product.thumbnail[0]}" target="_blank">${product.thumbnail[0]
+                    ? `<svg xmlns="http://www.w3.org/2000/svg" width="45" height="35" fill="currentColor" class="bi bi-card-image" color="blue" viewBox="0 0 16 16"><path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/><path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54L1 12.5v-9a.5.5 0 0 1 .5-.5z"/></svg>`
+                    : "---"}</a></td>
                     <td class="price update" id="${product._id}">${product.price}</td>
                     <td class="stock update" id="${product._id}">${product.stock}</td>
                     <td><a class="available" id="${product._id}">${availabilityIcon}</a></td>
