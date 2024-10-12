dd3c12404021eaf29fbfdbbf79d69093d8c6ab6f
lubiano83 <lubiano83@Mac-mini-de-Jose.local>
2024-10-11 21:24:24 -0300

    agregando tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './src/views/layouts/*.handlebars',   // Si usas Handlebars
+      './src/views/*.handlebars',  // Si usas HTML
+      './src/public/*.js',       // Si usas archivos JS o JSX
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
