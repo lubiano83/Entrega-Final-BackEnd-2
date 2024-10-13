1776fe3a62b7f77ca3e472f1b438c08ce182091c
lubiano83 <lubiano83@Mac-mini-de-Jose.local>
2024-10-13 18:31:51 -0300

    ajustes varios

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -4,10 +4,11 @@ import UserController from "../controllers/user.controller.js";
 
 const ROUTER = Router();
 const userController = new UserController();
+const permissions = passport.authenticate("current", { session: false });
 
 ROUTER.post("/register", userController.registerUser);
 ROUTER.post("/login", userController.loginUser);
 ROUTER.post("/logout", userController.logoutUser);
-ROUTER.get("/current", passport.authenticate("current", { session: false }), userController.currentUser);
+ROUTER.get("/current", permissions, userController.currentUser);
 
 export default ROUTER;
