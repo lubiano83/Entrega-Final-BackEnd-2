55a94b5911a451fbfdb81978772b14d5e505ed42
lubiano83 <lubiano83@Mac-mini-de-Jose.local>
2024-11-11 15:02:22 -0300

    ajustes varios

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -3,8 +3,15 @@ import { Schema, model } from "mongoose";
 const cartsCollection = "carts";
 
 const cartItemSchema = new Schema({
-    id: { type: Schema.Types.ObjectId, ref: "products", required: true },
-    quantity: { type: Number, required: true },
+    id: {
+        type: Schema.Types.ObjectId,
+        ref: "products",
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
     _id: false, // Esto para que no aparezca el _id.
 });
 
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -4,14 +4,51 @@ import paginate from "mongoose-paginate-v2";
 const productCollection = "products";
 
 const productSchema = new Schema({
-    category: { type: String, required: true, uppercase: true, trim: true, index: { name: "idx_category" } },
-    title: { type: String, required: true, uppercase: true, trim: true, index: { name: "idx_title" } },
-    description: { type: String, lowercase: true, required: true, trim: true },
-    price: { type: Number, required: true },
-    thumbnail: { type: Array, required: true },
-    code: { type: String, required: true, lowercase: true, unique: true, trim: true, index: { name: "idx_code" } },
-    stock: { type: Number, required: true },
-    available: { type: Boolean, default: true, index: { name: "idx_available" } },
+    category: {
+        type: String,
+        required: true,
+        uppercase: true,
+        trim: true,
+        index: { name: "idx_category" },
+    },
+    title: {
+        type: String,
+        required: true,
+        uppercase: true,
+        trim: true,
+        index: { name: "idx_title" },
+    },
+    description: {
+        type: String,
+        lowercase: true,
+        required: true,
+        trim: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    thumbnail: {
+        type: Array,
+        required: true,
+    },
+    code: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        trim: true,
+        index: { name: "idx_code" },
+    },
+    stock: {
+        type: Number,
+        required: true,
+    },
+    available: {
+        type: Boolean,
+        default: true,
+        index: { name: "idx_available" },
+    },
 }, { versionKey: false, toJSON: { virtuals: false }, toObject: { virtuals: false } });
 
 productSchema.index({ category: 1, title: 1 }, { name: "idx_category_title" });
